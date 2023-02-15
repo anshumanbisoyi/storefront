05c230649fbee77e1c3dfc76ec8cb7b0881da5fa
Anshuman Bisoyi <ansuman746@gmail.com>
2023-02-16 03:01:49 +0530

    send html to url

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
-#request -> response
-# request handler
 
 def say_hello(request):
-    return HttpResponse('Hello World')
-    #Pull data from db
-    # Transform
-    # Send Email
+    return render(request, 'hello.html', {'name':'Ansh'}) 
